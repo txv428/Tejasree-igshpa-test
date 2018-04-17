bbf2cd0cd611a1ece58f3dcc1bdd081404d267d0
txv428 <tejasree.vangapelli@gmail.com>
2018-04-17 11:04:50 -0500

    Update teja-fruits.py

--- teja-fruits.py
+++ teja-fruits.py
@@ -1,14 +1,16 @@
 import json
 from sets import *
 
+#read the data from fruits.json
 with open('fruits.json') as f:
   read_data = f.read()
 
-
+#data read is copied in data
 data = json.loads(read_data)
 
 frequencies = {}
 
+#Here the fequency of fruits repeated is counted
 for it in data:
   k = frozenset(set(it['fruits_picked']))
   if k in frequencies:
@@ -16,6 +18,7 @@ for it in data:
   else:
     frequencies[k] = 1
 
+#The top 20 most frequent combination of fruits
 index = 1
 for key, value in sorted(frequencies.iteritems(), key=lambda(k,v): (v,k), reverse=True):
     print "%s: %s" % (key, value)
